98cabe1c2afd90e67a777d39d8df155b82fcda74
Scott Cox <jscox5@gmail.com>
2022-05-26 17:33:38 -0600

    Model complete

--- Pokedex/Pokedex/Model/Pokemon.swift
+++ Pokedex/Pokedex/Model/Pokemon.swift
@@ -8,14 +8,13 @@
 import Foundation
 
 class Pokemon {
-    
     enum Keys: String {
-        case kName = "name"
-        case kId = "id"
-        case kMoves = "moves"
-        case kSprites = "sprites"
-        case kFrontShiny = "front_shiny"
-        case kMove = "move"
+        case name = "name"
+        case id = "id"
+        case moves = "moves"
+        case sprites = "sprites"
+        case frontShiny = "front_shiny"
+        case move = "move"
     }
     
     let name: String
@@ -23,32 +22,32 @@ class Pokemon {
     let moves: [String]
     let spritePath: String
     
-    init(name: String, id: Int, moves: [String], spritePath: String) {
+   
+    init(name:String, id:Int, moves:[String], spritePosterPath: String){
         self.name = name
         self.id = id
         self.moves = moves
-        self.spritePath = spritePath
+        self.spritePath = spritePosterPath
+        
     }
-} // End of class
-    
-    extension Pokemon {
-        convenience init?(dictionary: [String:Any]) {
-        guard let name = dictionary[Keys.kName.rawValue] as? String,
-            let id = dictionary[Keys.kId.rawValue] as? Int,
-            let spriteDict = dictionary[Keys.kSprites.rawValue] as? [String:Any],
-              let spritePosterPath = dictionary[Keys.kFrontShiny.rawValue] as? String,
-            let movesArray = dictionary[Keys.kMoves.rawValue] as? [[String:Any]]
-            else {return nil}
+}
+
+extension Pokemon {
+    convenience init?(dictionary:[String:Any]) {
+        guard let name = dictionary[Keys.name.rawValue] as? String,
+              let id = dictionary[Keys.id.rawValue] as? Int,
+              let spriteDict = dictionary[Keys.sprites.rawValue] as? [String:Any],
+              let spritePosterPath = spriteDict[Keys.frontShiny.rawValue] as? String,
+              let movesArray = dictionary[Keys.moves.rawValue] as? [[String:Any]] else {return nil}
+        
+        var moves: [String] = []
+        
+        for dict in movesArray {
+            guard let moveDict = dict[Keys.move.rawValue] as? [String:Any],
+                  let moveName = moveDict[Keys.name.rawValue] as? String else {return nil}
             
-            var moves: [String] = []
-            
-            for dict in movesArray {
-                guard let moveDict = dict[Keys.kMove.rawValue] as? [String:Any],
-                      let moveName = dict[Keys.kName.rawValue] as? String else {return nil}
-                        
             moves.append(moveName)
-                
-            self.init(name: name, id: id, moves: movesArray, spritePath: spritePosterPath)
-            }
         }
+        self.init(name: name, id: id, moves:moves, spritePosterPath: spritePosterPath)
+    }
 }
