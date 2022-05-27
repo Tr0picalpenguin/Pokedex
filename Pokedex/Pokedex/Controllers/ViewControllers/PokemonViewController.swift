abd187dc4bec8e0fe0edc23318536eb9eee74977
Scott Cox <jscox5@gmail.com>
2022-05-26 19:45:00 -0600

    final

--- Pokedex/Pokedex/Controllers/ViewControllers/PokemonViewController.swift
+++ Pokedex/Pokedex/Controllers/ViewControllers/PokemonViewController.swift
@@ -17,6 +17,46 @@ class PokemonViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pokemonMovesTableView.delegate = self
+        pokemonMovesTableView.dataSource = self
+        pokemonSearchBar.delegate = self
+    }
+    
+    var pokemon: Pokemon?
+    
+    func updateViews(for pokemon: Pokemon) {
+        NetworkController.fetchImage(pokemon: pokemon) { image in
+            guard let image = image else {return}
+            DispatchQueue.main.async {
+                self.pokemon = pokemon
+                self.pokemonIDLabel.text = "\(pokemon.id)"
+                self.pokemonNameLabel.text = pokemon.name.capitalized
+                self.pokemonSpriteImageView.image = image
+                self.pokemonMovesTableView.reloadData()
+            }
+        }
     }
 }// End
 
+extension PokemonViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemon?.moves.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "moveCell", for: indexPath)
+        guard let pokemon = pokemon else {return UITableViewCell()}
+        let move =  pokemon.moves[indexPath.row]
+        cell.textLabel?.text = move.capitalized
+        return cell
+    }
+}
+
+extension PokemonViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        NetworkController.fetchPokemon(searchTerm: searchText) { pokemon in
+            guard let pokemon = pokemon else {return}
+            self.updateViews(for: pokemon)
+        }
+    }
+}
